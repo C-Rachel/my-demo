ea47261a1ff2d1894b8912803ee91dcbe4a354cd
C-Rachel <c_rachel@sina.cn>
2019-01-23 15:35:43 +0800

    up

--- src/view/about.vue
+++ src/view/about.vue
@@ -2,10 +2,11 @@
 	<div>
 		<xheader></xheader>
 		<div class="div2">
+			<p>关于input监听计算</p>
 			<form>
 				<div class="group-box">
 					<label>Total</label>
-					<input v-model="meta.total" />
+					<input v-model="meta.total" disabled="disabled" />
 				</div>
 				<div class="group-box">
 					<label>1</label>
@@ -18,15 +19,15 @@
 				<!-- v-model.lazy即input失去焦点之后 -->
 				<div class="group-box">
 					<label>lazyTotal</label>
-					<input v-model="lazy.lllazy" />
+					<input v-model="lazy.lllazy" disabled="disabled" />
 				</div>
 				<div class="group-box">
 					<label>A</label>
-					<input v-model="lazy.bb" />
+					<input v-model.lazy="lazy.bb" />
 				</div>
 				<div class="group-box">
 					<label>B</label>
-					<input v-model="lazy.zz" />
+					<input v-model.lazy="lazy.zz" />
 				</div>
 			</form>
 		</div>
@@ -48,6 +49,7 @@
 		display: block;
 		color: #fff;
 		font-size: 20px;
+		margin-bottom: 5px;
 	}
 	input {
 		border: 0;
@@ -92,18 +94,21 @@
 				
 			},
 			getData: function(){
-				var self = this;
-				
+				var self = this;				
 				self.meta.total = parseInt(self.meta.ttt)+parseInt(self.meta.aaa);
-				console.log(self.meta)
+				
+				// 如果数据为小数则可以用parseFloat()，保留小数位数可以使用toFixed()
+				// 例如需要保留两位小数： var sum = (parseFloat(xx)+parseFloat(zz)).toFixed(2) 四舍五入保留
 			},
 			getA: function(){
-				
+				var self = this;				
+				self.lazy.lllazy = parseInt(self.lazy.bb)+parseInt(self.lazy.zz);
 			}
 		},
 		created: function(){
 			this.getData();
-			this.unwatch_total = this.$watch('meta', this.getData, {deep: true}); 			
+			this.unwatch_total = this.$watch('meta', this.getData, {deep: true}); 		
+			this.unwatch_lazy = this.$watch('lazy', this.getA, {deep: true}); 
 		},
 	};
 </script>

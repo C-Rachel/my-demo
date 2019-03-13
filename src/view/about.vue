1cfc5a432dd3abc3231e1fe1edca981f5fd40d62
C-Rachel <c_rachel@sina.cn>
2019-03-13 15:13:11 +0800

    uup

--- src/view/about.vue
+++ src/view/about.vue
@@ -30,6 +30,43 @@
 					<input v-model.lazy="lazy.zz" />
 				</div>
 			</form>
+			<div class="question-box" v-if="pageList[0]">
+				<div class="btnBox checkbox-btn-group btn-group clearfix">
+					<div class="btn-list">
+						<input type="checkbox" id="check_btn1" value="2" v-model="list" style="display: none;">
+						<label for="check_btn1" @click="getCheckbox()">checkbox1</label>
+					</div>
+					<div class="btn-list">
+						<input type="checkbox" id="check_btn2" value="3" v-model="list" style="display: none;">
+						<label for="check_btn2" @click="getCheckbox()">checkbox2</label>
+					</div>
+					<div class="btn-list">
+						<input type="checkbox" id="check_btn3" value="4" v-model="list" style="display: none;">
+						<label for="check_btn3" @click="getCheckbox()">checkbox3</label>
+					</div>				
+				</div>
+				<button v-bind:class="{ no_next: noNext }" class="next-btn text-uppercase" @click="next(0)" v-bind:disabled="isDisabled">Next</button>
+			</div>
+			<div class="question-box" v-if="pageList[1]">
+				<div class="btnBox radio-btn-group btn-group clearfix">
+					<div class="btn-list">
+						<input type="radio" id="redio_btn1" value="0" v-model="btnRadio[0]" style="display: none;">
+						<label for="redio_btn1" @click="getRadio()">嘻嘻嘻</label>
+					</div>
+					<div class="btn-list">
+						<input type="radio" id="redio_btn2" value="1" v-model="btnRadio[0]" style="display: none;">
+						<label for="redio_btn2" @click="getRadio()">哈哈哈</label>
+					</div>
+					<div class="btn-list">
+						<input type="radio" id="redio_btn3" value="2" v-model="btnRadio[0]" style="display: none;">
+						<label for="redio_btn3" @click="getRadio()">嘿嘿嘿嘿</label>
+					</div>			
+				</div>
+				<button v-bind:class="{ no_next: noNext }" class="next-btn text-uppercase" @click="next(1)" v-bind:disabled="isDisabled">Next</button>
+			</div>
+			<div class="question-box" v-if="pageList[2]">
+				<p class="texttext">Thank u</p>
+			</div>
 		</div>
 	</div>
 
@@ -71,6 +108,93 @@
 	.group-box {
 		margin-top: 20px;
 	}
+	.btnBox {
+		width: 100%;
+	}
+	.texttext {
+		margin-top: d-vw(35px);
+		color: #fff;
+	}
+	.question-box button {
+		&.active,
+		&:hover {
+			background: #0d133a;
+			color: #ffffff;
+		}
+	}
+	.btn-group {
+	    margin: d-vw(35px) auto 0;
+	    text-align: left;
+	    color: #16192e;
+	    .active {
+	        background-color: #16192e;
+	        color: #fff;
+	    }
+	    .btn-list {
+	        float: left;
+	        margin-right: d-vw(30px);
+	        margin-bottom: d-vw(20px);
+	    }
+	    .btn-list:nth-child(3) {
+	        margin-right: 0;
+	    }
+	}	
+	.checkbox-btn-group [type="checkbox"]:checked~label {
+	    background-color: #0d133a;
+	    color: #fff;
+	}	
+	.checkbox-btn-group label {
+	    cursor: pointer;
+	    width: d-vw(200px);
+	    border: 1px solid #16192e;
+	    background-color: #fff;
+	    font-size: d-vw(24px);
+	    padding: d-vw(10px) 0;
+	    display: inline-block;
+	    color: #16192e;
+	    text-align: center;
+	    box-sizing: border-box;
+	}	
+	.checkbox-btn-group label:hover {
+	    background: #0d133a;
+	    color: #ffffff;
+	}
+	.radio-btn-group [type="radio"]:checked~label {
+	    background-color: #0d133a;
+	    color: #fff;
+	}	
+	.radio-btn-group label {
+	    cursor: pointer;
+	    width: d-vw(315px);
+	    border: 1px solid #16192e;
+	    background-color: #fff;
+	    font-size: d-vw(24px);
+	    padding: d-vw(10px) 0;
+	    display: inline-block;
+	    text-align: center;
+	    box-sizing: border-box;
+		color: #16192e;
+	}	
+	.radio-btn-group label:hover {
+	    background: #0d133a;
+	    color: #ffffff;
+	}
+	.next-btn,
+	.no-next-btn {
+	    border: 1px solid #fff;
+	    background-color: #0d133a;
+	    font-size: d-vw(12px);
+	    width: d-vw(160px);
+	    padding: d-vw(15px) 0;
+	    color: #fff;
+	    letter-spacing: 4px;
+	    cursor: pointer;
+	    margin-top: d-vw(20px);
+	}
+	.div2 .question-box .no_next {
+	    background-color: #a4a4ab;
+	    cursor: no-drop;
+	}
 </style>
 
 <script>
@@ -92,7 +216,20 @@
 					"lllazy": "0.00",
 					"bb": "0.00",
 					"zz": "0.00",
-				}
+				},
+				pageList: {
+					0: 1,
+					1: 0,
+					2: 0
+				},
+				isDisabled: true,
+				noNext: true,
+				list: [],
+				btnRadio: {
+					0: "",
+					1: "",
+					2: ""
+				},
 			};
 		},
 		methods: {
@@ -107,6 +244,28 @@
 				var self = this;	
 				// 如果数据为小数则可以用parseFloat()，保留小数位数可以使用toFixed() 四舍五入保留
 				self.lazy.lllazy = (parseFloat(self.lazy.bb)+parseFloat(self.lazy.zz)).toFixed(2);				
+			},
+			getCheckbox() {
+				var self = this;
+				setTimeout(function(){
+					self.isDisabled = false;
+					self.noNext = false;
+					if(self.list.length == 0) {
+						self.isDisabled = true;
+						self.noNext = true;
+					}
+				}, 0)
+			},
+			next(num) {				
+				this.pageList[1 + num] = 1;
+				this.pageList[num] = 0;
+				this.isDisabled = true;
+				this.noNext = true;
+			},
+			getRadio() {				
+				var self = this;
+				this.isDisabled = false;
+				this.noNext = false;
 			}
 		},
 		created: function(){
